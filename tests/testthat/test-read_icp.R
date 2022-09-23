a15f28fe58c0d0d21a76670913bf1580e6c73afe
bentrueman <benjamin.trueman@dal.ca>
2022-09-23 14:57:38 -0300

    clean up tests

--- tests/testthat/test-read_icp.R
+++ tests/testthat/test-read_icp.R
@@ -1,14 +1,17 @@
 
-file <- list.files(
-  path = system.file("extdata", package = "cwrshelpr"),
-  full.names = TRUE
-)
-
 test_that("read_icp() yields a tibble with the expected number of rows", {
+  file <- list.files(
+    path = system.file("extdata", package = "cwrshelpr"),
+    full.names = TRUE
+  )
   expect_equal(nrow(read_icp(file[1])), 1440)
 })
 
 test_that("read_icp() yields expected output", {
+  file <- list.files(
+    path = system.file("extdata", package = "cwrshelpr"),
+    full.names = TRUE
+  )
   out1 <- read_icp(file[1])
   out2 <- read_icp(file[2])
   expect_snapshot(out1)
--- tests/testthat/test-read_lims.R
+++ tests/testthat/test-read_lims.R
@@ -1,11 +1,10 @@
 
-file <- list.files(
-  path = system.file("extdata", package = "cwrshelpr"),
-  full.names = TRUE,
-  pattern = ".+\\.xlsx"
-)
-
 test_that("read_lims() returns expected output", {
+  file <- list.files(
+    path = system.file("extdata", package = "cwrshelpr"),
+    full.names = TRUE,
+    pattern = ".+\\.xlsx"
+  )
   out <- read_lims(file[3], work_order = "00A123456")
   expect_snapshot(out)
 })
